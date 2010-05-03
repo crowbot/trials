123f047a070579399016ce3b2125e26b973f97f7
louise <louise@8c40c60f-321d-0410-8cbf-a8d12664d7a1>
2010-05-03 17:08:06 +0000

    Base parser functions
    git-svn-id: http://www.louisecrow.com/svn/trials@354 8c40c60f-321d-0410-8cbf-a8d12664d7a1

---
+++ lib/url_parser.rb
@@ -0,0 +1,43 @@
+require 'httpclient'
+require 'hpricot'
+
+class UrlParser 
+  
+  def request_page(request_path)
+    sleep(0.5)
+    http_client = HTTPClient.new
+    url = "http://#{base_host}#{request_path}"
+    puts url
+    content = http_client.get_content(url)
+    return content
+  end
+  
+  def data_dir
+    "data/"
+  end
+   
+
+  def get_local_file(page)
+    page = page.gsub(/^\//, '')
+    request_path = "/#{page}"
+    local_path = "#{data_dir}#{page}.html"
+    local_file = File.exist?(local_path)   
+    return local_path if local_file 
+     
+    body = nil
+    puts "getting #{request_path}"
+    begin
+     content = request_page(request_path)
+    rescue
+     # 1 retry
+     puts "retrying #{request_path}"
+     content = request_page(request_path)
+    end
+    directory = File.dirname(local_path)
+    FileUtils.mkdir_p(directory)
+    f = File.open(local_path, 'w')
+    f.write(content)
+    f.close
+    local_path
+  end
+end
