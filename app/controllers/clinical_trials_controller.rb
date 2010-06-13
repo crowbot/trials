3e39eee2592416be1d1ef5dbaa44ec02afd2a814
Louise Crow <louise.crow@gmail.com>
2010-06-13 17:56:40 +0100

    Slighly nicer text

--- app/controllers/clinical_trials_controller.rb
+++ app/controllers/clinical_trials_controller.rb
@@ -3,6 +3,7 @@ class ClinicalTrialsController < ApplicationController
   def index
     @trials = ClinicalTrial.completed.searched.unpublished.paginate(:all, :page => params[:page], 
                  :conditions => ['completion_date_as_date < ?', Time.now.beginning_of_month - 3.years])
+    @total_trials_count = ClinicalTrial.count
   end
   
   def show
