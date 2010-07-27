399735ba989928217c6991400c140f0631ab9ce6
Louise Crow <louise.crow@gmail.com>
2010-07-27 20:39:51 +0100

    Add sponsor filter

--- app/controllers/clinical_trials_controller.rb
+++ app/controllers/clinical_trials_controller.rb
@@ -1,8 +1,16 @@
 class ClinicalTrialsController < ApplicationController
   
   def index
+    condition_string = 'completion_date_as_date < ?'
+    condition_params = [Time.now.beginning_of_month - 3.years]
+    
+    if params[:agency_id]
+      condition_string += " and sponsors.agency_id = ?"
+      condition_params << params[:agency_id]
+    end
+    condition_params = [condition_string] + condition_params
     @trials = ClinicalTrial.completed.searched.unpublished.paginate(:all, :page => params[:page], 
-                 :conditions => ['completion_date_as_date < ?', Time.now.beginning_of_month - 3.years])
+                 :conditions => condition_params, :include => :sponsors)
     @total_trials_count = ClinicalTrial.count
   end
    
