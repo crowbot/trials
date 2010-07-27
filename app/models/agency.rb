c6622119eda9b279a028ef57a4b59537571b8a13
Louise Crow <louise.crow@gmail.com>
2010-07-27 20:03:29 +0100

    Memoize the agency function

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,25 +1,29 @@
 class Agency < ActiveRecord::Base
   has_many :sponsors
   has_many :clinical_trials, :through => :sponsors
+
+  class << self
+    extend ActiveSupport::Memoizable
   
-  def self.by_unpublished_completed(limit=10)
-    find_by_sql(["select agencies.*, cnt
-                 FROM agencies, (SELECT agency_id, count(*) as cnt
-                                 FROM sponsors 
-                                 WHERE clinical_trial_id in 
-                                      (SELECT clinical_trials.id 
-                                      FROM `clinical_trials` 
-                                      LEFT OUTER JOIN trial_mentions       
-                                      ON clinical_trials.id = trial_mentions.clinical_trial_id 
-                                      WHERE completion_date_as_date < ? 
-                                      AND trial_mentions.id is NULL
-                                      AND searched = 1
-                                      AND completion_date is not null)
-                                      GROUP BY agency_id) as tmp
-                  WHERE agencies.id = agency_id 
-                  ORDER BY cnt
-                  LIMIT #{limit};", Time.now.beginning_of_month - 3.years])
-    
+    def by_unpublished_completed(limit=10)
+      find_by_sql(["select agencies.*, cnt
+                   FROM agencies, (SELECT agency_id, count(*) as cnt
+                                   FROM sponsors 
+                                   WHERE clinical_trial_id in 
+                                        (SELECT clinical_trials.id 
+                                        FROM `clinical_trials` 
+                                        LEFT OUTER JOIN trial_mentions       
+                                        ON clinical_trials.id = trial_mentions.clinical_trial_id 
+                                        WHERE completion_date_as_date < ? 
+                                        AND trial_mentions.id is NULL
+                                        AND searched = 1
+                                        AND completion_date is not null)
+                                        GROUP BY agency_id) as tmp
+                    WHERE agencies.id = agency_id 
+                    ORDER BY cnt desc
+                    LIMIT #{limit};", Time.now.beginning_of_month - 3.years])
+    end
+    memoize :by_unpublished_completed
   end
   
 end
