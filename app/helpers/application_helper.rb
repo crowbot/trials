41019324f3725a76da3201ff1bd6726c3f38c858
Louise Crow <louise.crow@gmail.com>
2010-07-27 20:15:33 +0100

    Slightly nicer trial text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def trial_text trial
+    text = "#{trial.brief_title}"
+    if trial.completion_date
+      text += " (completed #{trial.completion_date})"
+    end
+    text
+  end 
+  
 end
