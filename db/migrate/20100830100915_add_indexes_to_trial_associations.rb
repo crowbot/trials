7a59f93d59bb5c33fefdd2464c69229c94a362fe
Louise Crow <louise.crow@gmail.com>
2010-08-30 11:16:50 +0100

    Adding indexes to association fields on tables associated to clinical trials

---
+++ db/migrate/20100830100915_add_indexes_to_trial_associations.rb
@@ -0,0 +1,29 @@
+class AddIndexesToTrialAssociations < ActiveRecord::Migration
+  def self.up
+    add_index :trial_interventions, :intervention_id
+    add_index :trial_interventions, :clinical_trial_id
+    add_index :condition_trials, :condition_id
+    add_index :condition_trials, :clinical_trial_id
+    add_index :overall_officials, :agency_id
+    add_index :overall_officials, :clinical_trial_id
+    add_index :trial_mentions, :article_id
+    add_index :trial_mentions, :clinicial_trial_id
+    add_index :responsible_parties, :clinical_trial_id
+    add_index :overseers, :authority_id
+    add_index :overseers, :clinical_trial_id
+  end
+
+  def self.down
+    remove_index :trial_interventions, :intervention_id
+    remove_index :trial_interventions, :clinical_trial_id
+    remove_index :condition_trials, :condition_id
+    remove_index :condition_trials, :clinical_trial_id
+    remove_index :overall_officials, :agency_id
+    remove_index :overall_officials, :clinical_trial_id
+    remove_index :trial_mentions, :article_id
+    remove_index :trial_mentions, :clinicial_trial_id
+    remove_index :responsible_parties, :clinical_trial_id
+    remove_index :overseers, :authority_id
+    remove_index :overseers, :clinical_trial_id
+  end
+end
